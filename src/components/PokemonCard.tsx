9e41ca2cf2e50662359df52e8c4e11620928d3fa
ELODY FORT <elody.fort.professionnel@gmail.com>
2024-10-22 11:43:56 +0200

    exercice02

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+  return (
+    <section>
+      <h1>Hello React</h1>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="pokemon "
+        />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </section>
+  );
+}
+export default PokemonCard;
